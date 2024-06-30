56cf35f95bb72c02554f4d6991811582acf45a49
Cold927 <colddevelop@gmail.com>
2024-06-30 11:37:59 +0300

    Initial commit

---
+++ main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
+	"log"
+	_ "time-tracker/docs"
+)
+
+// @title Time Tracker API
+// @version 1
+// @Description Документация по сервису тайм трекера
+
+// @host localhost:8500
+// @BasePath /api/v1
+func main() {
+	router := gin.Default()
+	people := router.Group("/users")
+	tasks := router.Group("/tasks")
+
+	user := people.Group("/users")
+	{
+		user.POST("/create")
+		user.GET("/list")
+		user.GET("/list/:id")
+
+		user.DELETE("/delete")
+	}
+
+	task := tasks.Group("/tasks")
+	{
+		task.PATCH("/countdown/start")
+		task.PATCH("/countdown/end")
+	}
+	router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+
+	log.Fatal(router.Run(":8500")) // #TODO засунуть все переменные в .env
+}
